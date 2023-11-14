b65a8e821e3b9cbf3eef945f1f697494d829481d
NunezGabriel <Tugabriel.nunez.arenas@gmail.com>
2023-11-14 17:38:22 -0500

    add: Principal page -- page

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="w-full h-[56px] bg-[#034752] flex items-center justify-center drop-shadow-lg">
-        <div class="flex justify-between max-w-[1600px] w-full px-5 md:px-7">
+        <div class="flex justify-between max-w-[2000px] w-full px-5 md:px-7">
             <h1 class="text-[#e6edee]">TEKTON UNIVERSITY</h1>
-            <button class="text-[#e6edee] md:hover:text-[#66e7da]">
+            <button class="text-[#e6edee] transition ease-in duration-100 md:hover:text-[#66e7da]">
                 Go Back
             </button>
         </div>
--- src/pages/PrincipalPage.vue
+++ src/pages/PrincipalPage.vue
@@ -1,10 +1,14 @@
 <template>
     <NavComponent/>
-    <section class="w-[300px] mx-auto mt-10 flex flex-col gap-8">
-        <div class="bg-white w-full h-[250px]  drop-shadow-2xl">
+    <section class="w-[300px] md:w-[800px] md:flex-row md:mt-[200px] mx-auto mt-10 flex flex-col gap-8">
+        <div class="transition ease-in-out md:hover:-translate-y-3 font-bold bg-white w-full h-[250px] drop-shadow-2xl gap-3 flex-col flex justify-center items-center">
+            <img class="h-[150px] w-[150px]" :src="imagen" alt="student image">
+            <h1>STUDENT</h1>
         </div>
 
-        <div class="bg-white w-full h-[250px]  drop-shadow-2xl">
+        <div class="transition ease-in-out md:hover:-translate-y-3 font-bold bg-white w-full h-[250px] drop-shadow-2xl gap-3 flex-col flex justify-center items-center">
+            <img class="h-[150px] w-[150px]" :src="imagen2" alt="admin image">
+            <h1>ADMIN</h1>
         </div>
     </section>
 </template>
@@ -15,6 +19,12 @@
     name: "PrincipalPage",
     components: {
         NavComponent
-    }
+    },
+    data() {
+    return {
+      imagen: require('../assets/student.png'),
+      imagen2: require('../assets/admin.png')
+    };
+  }
 }
 </script>
