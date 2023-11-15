a70d111e3ab5de01dbae980cbf849bead723f883
NunezGabriel <Tugabriel.nunez.arenas@gmail.com>
2023-11-14 23:19:42 -0500

    add: project like before

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="w-full h-[56px] bg-[#034752] flex items-center justify-center drop-shadow-lg">
+        <div class="flex justify-between max-w-[2000px] w-full px-5 md:px-7">
+            <h1 class="text-[#e6edee]">TEKTON UNIVERSITY</h1>
+            <button class="text-[#e6edee] transition ease-in duration-100 md:hover:text-[#66e7da]">
+                Go Back
+            </button>
+        </div>
+    </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,28 @@
+<script setup>
+    import NavBarComponent from "../components/NavBarComponent.vue"
+</script>
+
 <template>
-    <h1 class="text-red-700">holaaa</h1>
+    <NavBarComponent/>
+    <section class="w-[300px] md:w-[800px] md:flex-row md:mt-[200px] mx-auto mt-10 flex flex-col gap-8">
+    <div class="transition ease-in-out md:hover:-translate-y-3 font-bold bg-white w-full h-[250px] drop-shadow-2xl gap-3 flex-col flex justify-center items-center">
+        <img class="h-[150px] w-[150px]" src="../assets/student.png" alt="student image">
+        <h1>STUDENT</h1>
+    </div>
+
+    <div class="transition ease-in-out md:hover:-translate-y-3 font-bold bg-white w-full h-[250px] drop-shadow-2xl gap-3 flex-col flex justify-center items-center">
+        <img class="h-[150px] w-[150px]" src="../assets/admin.png" alt="admin image">
+        <h1>ADMIN</h1>
+    </div>
+</section>
 </template>
+
+
+<!-- export default {
+    data() {
+    return {
+      imagen: require('../assets/student.png'),
+      imagen2: require('../assets/admin.png')
+    };
+  }
+} -->
